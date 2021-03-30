8d99966e307764c0011593d81bb701e140977af5
Pau Mezzadra <paumezzadra@hotmail.com>
2021-03-30 14:27:50 -0300

    Add project files.

---
+++ FechasEjercicio21/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FechasEjercicio21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"La fecha de hoy es {DateTime.Now}");
+            Console.ReadKey();
+        }
+    }
+}
